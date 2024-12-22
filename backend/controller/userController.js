995a69f93e1c180fa25b27c5bc72d7db312a97d0
Vikhyath-Shetty <vikhyath.v.shetty@gmail.com>
2024-12-22 14:08:12 +0530

    completed userRegisteration

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,17 +1,38 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  const { username, type, phone_number, email, password } = req.body;
-  if (!username || !type || !phone_number || !email || !password) {
+  const { username, type, phone, email, password } = req.body;
+  if (!username || !type || !phone || !email || !password) {
     res.status(400);
     throw new Error("All fields are mandatory");
   }
-  res.json({ message: "registration was successfull" });
-});
+  const userAvailable = await User.findOne({ email });
 
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username,
+    type,
+    phone,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json(user);
+  } else {
+    res.status(400);
+    throw new Error("user data is invalid");
+  }
+});
 
 //@desc Login a user
 //@route POST /api/users/login
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  username:{
-    type:String,
-    required:[true,"please enter username"],
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "please enter username"],
+    },
+    type: {
+      type: String,
+      required: [true, "please enter the type field"],
+    },
+    phone: {
+      type: String,
+      required: [true, "please enter phone number"],
+    },
+    email: {
+      type: String,
+      required: [true, "please enter email"],
+    },
+    password: {
+      type: String,
+      required: [true, "please enter password"],
+    },
   },
-  type:{
-    type:String,
-    required:[true,"please enter the type field"]
-  },
-  phone:{
-    type:String,
-    required:[true,"please enter phone number"],
-  },
-  email:{
-      type:String,
-      required:[true,"please enter email"],
-  },
-  password:{
-    type:String,
-    required:[true,"please enter password"],
+  {
+    timestamps: true,
   }
-});
+);
 
-module.exports = mongoose.model("Users",userSchema);
+module.exports = mongoose.model("User", userSchema);
